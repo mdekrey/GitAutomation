bbb3180a311242994f05f5a8915c24aba4681820
Matt DeKrey <mattdekrey@gmail.com>
2017-08-15 07:42:10 -0500

    Style log

--- GitAutomation/web-scripts/logs/log.presentation.ts
+++ GitAutomation/web-scripts/logs/log.presentation.ts
@@ -10,6 +10,44 @@ export const logPresentation: IRxBindProps<
   element: "li",
   selector: "li",
   onEach: selection => {
-    selection.text(data => JSON.stringify(data));
+    selection
+      .select(function(data) {
+        if (data.channel === OutputMessageChannel.Error) {
+          return this;
+        }
+        return null;
+      })
+      .style("font-weight", "bold")
+      .text(data => data.message);
+
+    selection
+      .select(function(data) {
+        if (data.channel === OutputMessageChannel.Out) {
+          return this;
+        }
+        return null;
+      })
+      .text(data => data.message);
+
+    selection
+      .select(function(data) {
+        if (data.channel === OutputMessageChannel.StartInfo) {
+          return this;
+        }
+        return null;
+      })
+      .style("font-style", "italic")
+      .text(data => data.message);
+
+    selection
+      .select(function(data) {
+        if (data.channel === OutputMessageChannel.ExitCode) {
+          return this;
+        }
+        return null;
+      })
+      .style("color", data => (data.exitCode ? "red" : "initial"))
+      .style("margin-bottom", `10px`)
+      .text(data => `Exit code: ${data.exitCode}`);
   }
 };
