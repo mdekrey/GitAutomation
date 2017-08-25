107cea043d52f8270dce09ed43a3914ba8c2b791
Matt DeKrey <mattdekrey@gmail.com>
2017-08-24 21:22:51 -0500

    Add tests for the remaining SQL scripts (and find one broken)

--- GitAutomation.Core.Tests/BranchSettings/SqlBranchSettingsShould.cs
+++ GitAutomation.Core.Tests/BranchSettings/SqlBranchSettingsShould.cs
@@ -16,10 +16,52 @@ namespace GitAutomation.BranchSettings
         }
 
         [TestMethod]
-        public void HaveAValidGetConfiguredBranchesCommand()
-        {
+        public void HaveAValidGetConfiguredBranchesCommand() =>
             SqlBranchSettings.GetConfiguredBranchesCommand.ExplainMultipleResult(database.BuildSqlConnection());
 
-        }
+
+        [TestMethod]
+        public void HaveAValidGetDownstreamBranchesCommand() =>
+            SqlBranchSettings.GetDownstreamBranchesCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidGetUpstreamBranchesCommand() =>
+            SqlBranchSettings.GetUpstreamBranchesCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidGetAllDownstreamBranchesCommand() =>
+            SqlBranchSettings.GetAllDownstreamBranchesCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidGetAllUpstreamBranchesCommand() =>
+            SqlBranchSettings.GetAllUpstreamBranchesCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidGetAllUpstreamRemovableBranchesCommand() =>
+            SqlBranchSettings.GetAllUpstreamRemovableBranchesCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidAddBranchPropagationCommand() =>
+            SqlBranchSettings.AddBranchPropagationCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidRemoveBranchPropagationCommand() =>
+            SqlBranchSettings.RemoveBranchPropagationCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidGetBranchBasicDetialsCommand() =>
+            SqlBranchSettings.GetBranchBasicDetialsCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidUpdateBranchSettingCommand() =>
+            SqlBranchSettings.UpdateBranchSettingCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidDeleteBranchSettingsCommand() =>
+            SqlBranchSettings.DeleteBranchSettingsCommand.ExplainMultipleResult(database.BuildSqlConnection());
+
+        [TestMethod]
+        public void HaveAValidConsolidateServiceLineCommand() =>
+            SqlBranchSettings.ConsolidateServiceLineCommand.ExplainMultipleResult(database.BuildSqlConnection());
     }
 }
