37b909b0b7b3e5b8f3d73fb7e06cb4e7dea1d4d5
Matt DeKrey <mattdekrey@gmail.com>
2017-09-02 13:13:29 -0500

    Add SQL permissions

---
+++ GitAutomation.Sql/UserRole.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserRole]
+(
+	[ClaimName] NVARCHAR(256) NOT NULL, 
+    [Role] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_UserRole_ToUser] FOREIGN KEY (ClaimName) REFERENCES [User](ClaimName), 
+    CONSTRAINT [PK_UserRole] PRIMARY KEY ([ClaimName], [Role]) 
+)
