f05f07268cad6acaea809effe7578ad6c1197f27
Matt DeKrey <mattdekrey@gmail.com>
2017-09-28 07:45:26 -0500

    Implement postgres database

---
+++ GitAutomation.Postgres/2-UpstreamBranch.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE UpstreamBranch
+(
+    DownstreamBranch VARCHAR(255) NOT NULL,
+    BranchName VARCHAR(255) NOT NULL,
+    CONSTRAINT PK_DownstreamBranch PRIMARY KEY (DownstreamBranch, BranchName),
+    CONSTRAINT FK_UpstreamBranch_ToDownstreamBranch FOREIGN KEY (DownstreamBranch) REFERENCES DownstreamBranch(BranchName)
+);
+
+CREATE INDEX IX_UpstreamBranch_BranchName ON UpstreamBranch (BranchName)
