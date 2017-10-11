d12d463c6468cd075546b25aad98912b41f79e00
Matt DeKrey <mattdekrey@gmail.com>
2017-10-10 20:09:58 -0500

    Write script to build tag from branch

---
+++ ci/branch-to-tag.sh
@@ -0,0 +1,19 @@
+PARTS=(${TRAVIS_BRANCH//\// })
+TAG=:dev
+if [ "${PARTS[0]}" == "rel" ]; then
+    TAG=:${PARTS[1]}
+fi;
+if [ "${PARTS[0]}" == "line" ]; then
+    TAG=:${PARTS[1]}
+fi
+if [ "${PARTS[0]}" == "rc" ]; then
+    MORE_PARTS=(${PARTS[1]//-/ })
+    REST=("${MORE_PARTS[@]:0:1}")
+    REST+=("rc")
+    if [ "${MORE_PARTS[1]}" != "" ]; then
+        REST+=("${MORE_PARTS[@]:1}")
+    fi
+    TAG=$(printf -- "-%s" "${REST[@]}")
+    TAG=:${TAG:1}
+fi
+echo $TAG
