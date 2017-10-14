c7ffbc40d4bb9b253269b837452b5043d0b7e853
Matt DeKrey <mattdekrey@gmail.com>
2017-10-14 12:01:06 -0500

    Refactor addon assembly loading

---
+++ GitAutomation/AddonFramework/AddonAssemblyLoader.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.DependencyModel.Resolution;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.DotNet.PlatformAbstractions;
+
+namespace GitAutomation.AddonFramework
+{
+    public class AddonAssemblyLoader : IDisposable
+    {
+        private const string dependencyWildcard = "*.deps.json";
+        private static readonly Regex dependencyExtension = new Regex(".deps.json$");
+
+        private readonly CompositeCompilationAssemblyResolver assemblyResolver;
+        private readonly IReadOnlyList<string> orderedRuntimes;
+        private readonly IReadOnlyList<(string dir, DependencyContext context)> dependencyContexts;
+
+        public AddonAssemblyLoader(string rootFolder)
+        {
+            if (Directory.Exists(rootFolder))
+            {
+                // Build the assembly resolver, which finds paths based on a RuntimeLibrary alone. Doesn't seem to check runtime dirs, though.
+                this.assemblyResolver = new CompositeCompilationAssemblyResolver(
+                    (from path in Directory.EnumerateDirectories(rootFolder)
+                     select new AppBaseCompilationAssemblyResolver(path) as ICompilationAssemblyResolver
+                    ).Concat(new ICompilationAssemblyResolver[]
+                    {
+                        new ReferenceAssemblyPathResolver(),
+                        new PackageCompilationAssemblyResolver()
+                    }).ToArray()
+                );
+
+                // Figure out our runtime monikers, including fallbacks. We want to use our specific runtime first, of course, but then the
+                // fallbacks are ordered in priority order. Lastly, if no runtime matches, we'll use the empty-string version.
+                var runtimeFallbacks = DependencyContext.Default.RuntimeGraph.FirstOrDefault(v => v.Runtime == RuntimeEnvironment.OperatingSystem);
+                this.orderedRuntimes = new[] { runtimeFallbacks.Runtime }.Concat(runtimeFallbacks.Fallbacks).Concat(new[] { "" }).ToList().AsReadOnly();
+
+                // Next, we find all the *.deps.json files. These say what depends on what, and gives us a dependency context. This is handy
+                // for a faster way to find the DLLs than scanning the folders. This could also be used to pre-load dependencies, but remember
+                // that not all dependencies are required for all code paths; lazy loading will be fine in case of this application.
+                var allFiles = Directory.EnumerateFiles(rootFolder, dependencyWildcard, SearchOption.AllDirectories).ToArray();
+                this.dependencyContexts =
+                    (from depFile in allFiles
+                     let dllPath = dependencyExtension.Replace(depFile, ".dll")
+                     let assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(dllPath)
+                     select (dir: Path.GetDirectoryName(dllPath), context: DependencyContext.Load(assembly))).ToArray();
+
+                // Register our resolution handler
+                AssemblyLoadContext.Default.Resolving += HandleResolving;
+            }
+        }
+
+        /// <summary>
+        /// Unregisters the resolution handler in case we no longer want to load new add-ons. Remember that all the old DLLs are still in memory;
+        /// we didn't put them into a separate AppContext in this implementation.
+        /// </summary>
+        void IDisposable.Dispose()
+        {
+            AssemblyLoadContext.Default.Resolving -= HandleResolving;
+        }
+        
+        private Assembly HandleResolving(AssemblyLoadContext context, AssemblyName name)
+        {
+            return
+                // either an already loaded assembly...
+                (from a in AppDomain.CurrentDomain.GetAssemblies()
+                 where a.GetName().FullName == name.FullName
+                 select a).FirstOrDefault()
+                // or loaded from our add-ons folder
+                ?? (from entry in dependencyContexts
+                    from library in entry.context.RuntimeLibraries
+                    where NamesMatch(library)
+                    from finalPath in PossibleAssemblyPaths(entry.dir, library)
+                    where File.Exists(finalPath)
+                    let assembly = TryLoadAssembly(finalPath)
+                    where assembly != null
+                    select assembly).FirstOrDefault();
+
+            bool NamesMatch(RuntimeLibrary runtime)
+            {
+                return string.Equals(runtime.Name, name.Name, StringComparison.OrdinalIgnoreCase);
+            }
+
+            Assembly TryLoadAssembly(string assemblyPath)
+            {
+                try
+                {
+                    return context.LoadFromAssemblyPath(assemblyPath);
+                }
+                catch (Exception ex)
+                {
+                    try
+                    {
+                        // An AssemblyLoadContext can cache that it couldn't find a file so it doesn't try again.
+                        // However, since we're specifying a path that we know already exists, we can trick it by
+                        // loading the bytes directly.
+                        // See https://stackoverflow.com/q/4483429/195653 for more information
+                        return Assembly.Load(File.ReadAllBytes(assemblyPath));
+                    }
+                    catch (Exception ex2)
+                    {
+                        Console.WriteLine(ex);
+                        Console.WriteLine(ex2);
+                        return null;
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<string> PossibleAssemblyPaths(string withinDirectory, RuntimeLibrary library)
+        {
+            return (from paths in PathsPrioritizedByRuntime()
+                    from assetPath in paths
+                    select Path.Combine(withinDirectory, assetPath))
+                .Concat(ResolveAssemblyPaths());
+
+            IEnumerable<IEnumerable<string>> PathsPrioritizedByRuntime()
+            {
+                return from runtime in orderedRuntimes
+                       let assetPaths = FindPathsMatchingRuntime(runtime)
+                       where assetPaths != null
+                       select assetPaths;
+            }
+
+            IEnumerable<string> FindPathsMatchingRuntime(string runtime)
+            {
+                return library.RuntimeAssemblyGroups.FirstOrDefault(g => g.Runtime == runtime)?.AssetPaths;
+            }
+
+            IReadOnlyList<string> ResolveAssemblyPaths()
+            {
+                var wrapper = new CompilationLibrary(
+                    library.Type,
+                    library.Name,
+                    library.Version,
+                    library.Hash,
+                    library.RuntimeAssemblyGroups.SelectMany(g => g.AssetPaths),
+                    library.Dependencies,
+                    library.Serviceable);
+                var assemblyPaths = new List<string>();
+                assemblyResolver.TryResolveAssemblyPaths(wrapper, assemblyPaths);
+                return assemblyPaths.AsReadOnly();
+            }
+        }
+    }
+}
--- GitAutomation/Program.cs
+++ GitAutomation/Program.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyModel.Resolution;
-using Microsoft.Extensions.DependencyModel;
-using System.Runtime.Loader;
-using System.Text.RegularExpressions;
 
 namespace GitAutomation
 {
@@ -17,94 +8,7 @@ namespace GitAutomation
     {
         public static void Main(string[] args)
         {
-            string rootFolder = "/extra-bins";
-            if (Directory.Exists(rootFolder))
-            {
-                var directories = Directory.EnumerateDirectories(rootFolder);
-                var resolvers = from path in directories
-                                select new AppBaseCompilationAssemblyResolver(path) as ICompilationAssemblyResolver;
-
-                var assemblyResolver = new CompositeCompilationAssemblyResolver(
-                    resolvers.Concat(new ICompilationAssemblyResolver[]
-                    {
-                        new ReferenceAssemblyPathResolver(),
-                        new PackageCompilationAssemblyResolver()
-                    }).ToArray()
-                );
-                var runtimeFallbacks = DependencyContext.Default.RuntimeGraph.FirstOrDefault(v => v.Runtime == Microsoft.DotNet.PlatformAbstractions.RuntimeEnvironment.OperatingSystem);
-                var orderedRuntimes = new[] { runtimeFallbacks.Runtime }.Concat(runtimeFallbacks.Fallbacks).Concat(new[] { "" }).ToArray();
-
-                var allFiles = Directory.EnumerateFiles(rootFolder, "*.deps.json", SearchOption.AllDirectories).ToArray();
-                var dependencyContexts = (from depFile in allFiles
-                                          let dllPath = Regex.Replace(depFile, ".deps.json$", ".dll")
-                                          let assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(dllPath)
-                                          select new { dir = Path.GetDirectoryName(dllPath), context = DependencyContext.Load(assembly) }).ToArray();
-                AssemblyLoadContext.Default.Resolving += (context, name) =>
-                {
-                    var asm = (from a in AppDomain.CurrentDomain.GetAssemblies()
-                               where a.GetName().FullName == name.FullName
-                               select a).FirstOrDefault();
-                    if (asm != null)
-                    {
-                        return asm;
-                    }
-
-                    var namePart = name.Name;
-                    bool NamesMatch(RuntimeLibrary runtime)
-                    {
-                        return string.Equals(runtime.Name, namePart, StringComparison.OrdinalIgnoreCase);
-                    }
-                    string ResolveAssemblyPaths(RuntimeLibrary library)
-                    {
-                        var wrapper = new CompilationLibrary(
-                            library.Type,
-                            library.Name,
-                            library.Version,
-                            library.Hash,
-                            library.RuntimeAssemblyGroups.SelectMany(g => g.AssetPaths),
-                            library.Dependencies,
-                            library.Serviceable);
-                        var assemblyPaths = new List<string>();
-                        assemblyResolver.TryResolveAssemblyPaths(wrapper, assemblyPaths);
-                        return assemblyPaths.FirstOrDefault();
-                    }
-                    Assembly TryLoadAssembly(string assemblyPath)
-                    {
-                        try
-                        {
-                            return context.LoadFromAssemblyPath(assemblyPath);
-                        }
-                        catch (Exception ex)
-                        {
-                            try
-                            {
-                                return Assembly.Load(File.ReadAllBytes(assemblyPath));
-                            }
-                            catch (Exception ex2)
-                            {
-                                Console.WriteLine(ex);
-                                Console.WriteLine(ex2);
-                                return null;
-                            }
-                        }
-                    }
-
-                    var resultAssembly = (from entry in dependencyContexts
-                                          from library in entry.context.RuntimeLibraries
-                                          where NamesMatch(library)
-                                          from runtime in orderedRuntimes
-                                          let asmGroup = library.RuntimeAssemblyGroups.FirstOrDefault(g => g.Runtime == runtime)
-                                          where asmGroup != null
-                                          from finalPath in asmGroup.AssetPaths.Select(assetPath => Path.Combine(entry.dir, assetPath))
-                                                               .Concat(new[] { ResolveAssemblyPaths(library) })
-                                          where File.Exists(finalPath)
-                                          where finalPath != null
-                                          let assembly = TryLoadAssembly(finalPath)
-                                          where assembly != null
-                                          select assembly).FirstOrDefault();
-                    return resultAssembly;
-                };
-            }
+            new AddonFramework.AddonAssemblyLoader("/extra-bins");
 
             var host = new WebHostBuilder()
                 .UseKestrel()
