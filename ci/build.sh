d255c1d5c1a6895d044211057869d9fbd414c5d6
Matt DeKrey <mattdekrey@gmail.com>
2017-10-26 21:26:05 -0500

    Correct build

--- ci/build.sh
+++ ci/build.sh
@@ -3,16 +3,18 @@
 # Intended to be run with cwd being the root of the project inside docker
 
 cd GitAutomation.GraphQL
+mkdir ../GitAutomation/generated
 dotnet run > ../GitAutomation/generated/graphql.json
 cd ..
 
 cd GitAutomation
 yarn
-yarn gql-generation
+cat generated/graphql.json | yarn gql-generation
 yarn build --env.NODE_ENV=production
 cd ..
 
-dotnet restore ./GitAutomation.sln && dotnet publish ./GitAutomation.sln -c ci-docker -o ./obj/Docker/publish
+dotnet restore ./GitAutomation.sln
+dotnet publish ./GitAutomation.sln -c ci-docker -o ./obj/Docker/publish
 
 cd GitAutomation.Postgres
 # Need to build the debug version to generate scripts
