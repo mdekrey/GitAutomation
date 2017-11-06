4d52969b70940208e684ff772d63ff2214fd47eb
Matt DeKrey <mattdekrey@gmail.com>
2017-11-06 07:36:20 -0600

    Better security

--- GitAutomation/web-scripts/index.ts
+++ GitAutomation/web-scripts/index.ts
@@ -3,6 +3,7 @@ import { Observable } from "./utils/rxjs";
 import { d3element } from "./utils/presentation/d3-binding";
 import {
   buildCascadingStrategy,
+  ICascadingRoutingStrategy,
   route,
   RouteConcrete,
   wildcard
@@ -13,7 +14,6 @@ import { homepage } from "./home/index";
 import { manage } from "./manage-branch/index";
 import { newBranch } from "./new-branch/index";
 import { currentClaims } from "./api/basics";
-import { ClaimDetails } from "./api/claim-details";
 import { login } from "./login/index";
 import "./style/global";
 import { admin } from "./admin/index";
@@ -25,6 +25,7 @@ const claims = currentClaims.publishReplay(1);
 claims.connect();
 
 buildCascadingStrategy(windowHashStrategy)
+  .let(handleSecurity)
   .map(
     route<RoutingComponent<never>>({
       "": RouteConcrete(homepage(body)),
@@ -47,22 +48,37 @@ buildCascadingStrategy(windowHashStrategy)
     }
   });
 
-buildCascadingStrategy(windowHashStrategy)
-  .map(
-    route<RoutingComponent<string>>({
-      login: RouteConcrete(() =>
-        claims
-          .filter<ClaimDetails>(claims => claims.roles.length !== 0)
-          .map(() => "/")
-      ),
-      [wildcard]: RouteConcrete(() =>
-        claims
-          .filter<ClaimDetails>(claims => claims.roles.length === 0)
-          .map(() => "/login")
-      )
+function handleSecurity(strategy: Observable<ICascadingRoutingStrategy<any>>) {
+  interface SecurityComponent {
+    redirectPath: string | null;
+    state: ICascadingRoutingStrategy<any>;
+  }
+  return strategy
+    .map(strat =>
+      route<RoutingComponent<SecurityComponent>>({
+        login: RouteConcrete<RoutingComponent<SecurityComponent>>(state =>
+          claims.map(claims => ({
+            redirectPath: claims.roles.length !== 0 ? "/" : null,
+            state: strat
+          }))
+        ),
+        [wildcard]: RouteConcrete<RoutingComponent<SecurityComponent>>(state =>
+          claims.map(claims => ({
+            redirectPath: claims.roles.length === 0 ? "/login" : null,
+            state: strat
+          }))
+        )
+      })(strat)
+    )
+    .switchMap(renderRouteOnce)
+    .do(v => {
+      if (v.redirectPath) {
+        windowHashStrategy.navigate({
+          url: v.redirectPath,
+          replaceCurentHistory: true
+        });
+      }
     })
-  )
-  .switchMap(renderRouteOnce)
-  .subscribe((url: string) =>
-    windowHashStrategy.navigate({ url, replaceCurentHistory: true })
-  );
+    .filter(v => !v.redirectPath)
+    .map(v => v.state);
+}
