ed47c0c9cf570cc75d6432772b7c2b1756ebf89b
Matt DeKrey <mattdekrey@gmail.com>
2019-05-13 19:01:52 -0500

    Begin repository scripts

---
+++ GitAutomation.Web/Scripts/Repository/gitBranchStates.ps1
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/env pwsh
+
+param(
+	[string] $repository,
+	[string] $password,
+	[string] $userEmail,
+	[string] $userName,
+	[string] $checkoutPath
+)
+
+$gitParams = Create-GitParams -password "$password" -userEmail "$userEmail" -userName "$userName" -checkoutPath "$checkoutPath"
+
+$result = With-Git $gitParams {
+	$allRefs = (git for-each-ref --format='%(refname:short) {%(objectname)}' refs/remotes | ForEach-Object -Process {
+		$r = [regex]::match($_, '^(?<name>.+) \{(?<commit>[0-9a-f]{40})\}')
+		return @{ "name" = $r.Groups['name'].Value; "commit" = $r.Groups['commit'].Value }
+	})
+}
+# TODO - send this as an action
