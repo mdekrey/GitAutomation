fe023e42275365565ee96ed917477b51268d1c36
Matt DeKrey <mattdekrey@gmail.com>
2019-05-18 16:35:45 -0500

    Ensure invocations get cancelled

--- GitAutomation.Web/Hubs/GraphQLHub.cs
+++ GitAutomation.Web/Hubs/GraphQLHub.cs
@@ -2,6 +2,7 @@
 using GitAutomation.Web.GraphQL;
 using GitAutomation.Web.State;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -17,18 +18,22 @@ namespace GitAutomation.Web.Hubs
     public class GraphQLHub : Hub
     {
         private readonly IStateMachine<AppState> stateMachine;
+        private readonly ILogger logger;
 
-        public GraphQLHub(IStateMachine<AppState> stateMachine)
+        public GraphQLHub(IStateMachine<AppState> stateMachine, ILogger<GraphQLHub> logger)
         {
             this.stateMachine = stateMachine;
+            this.logger = logger;
         }
 
-        public ChannelReader<string> Query(string graphql)
+        public ChannelReader<string> Query(string graphql, CancellationToken cancellation)
         {
             var ast = graphql.AsGraphQlAst();
-            var observable = stateMachine.StateUpdates.Select(e => ast.ToJson(e.State).ToString());
+            var observable = stateMachine.StateUpdates.Select(e => ast.ToJson(e.State).ToString())
+                .DistinctUntilChanged();
 
-            return observable.AsChannelReader(Context.ConnectionAborted);
+            var allCancellation = CancellationTokenSource.CreateLinkedTokenSource(cancellation, Context.ConnectionAborted);
+            return observable.AsChannelReader(allCancellation.Token);
         }
     }
 }
