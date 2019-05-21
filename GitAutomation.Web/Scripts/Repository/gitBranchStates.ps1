78de7c73f59adb3b68f042f96495c596663b3e5d
Matt DeKrey <mattdekrey@gmail.com>
2019-05-20 19:14:00 -0500

    Git branch cleanup for version stability

--- GitAutomation.Web/Scripts/Repository/gitBranchStates.ps1
+++ GitAutomation.Web/Scripts/Repository/gitBranchStates.ps1
@@ -6,8 +6,8 @@ param(
 )
 
 Push-Location "$checkoutPath"
-$allRefs = (git for-each-ref --format='%(refname:short) {%(objectname)}' refs/heads | ForEach-Object -Process {
-	$r = [regex]::match($_, '^(heads/)?(?<name>.+) \{(?<commit>[0-9a-f]{40})\}')
+$allRefs = (git for-each-ref --format='%(refname) {%(objectname)}' refs/heads | ForEach-Object -Process {
+	$r = [regex]::match($_, '^refs/heads/(?<name>.+) \{(?<commit>[0-9a-f]{40})\}')
 	return @{ "name" = $r.Groups['name'].Value; "commit" = $r.Groups['commit'].Value }
 })
 Build-StandardAction "TargetRepository:Refs" @{ "startTimestamp" = $startTimestamp; "allRefs" = $allRefs | ConvertTo-Json }
