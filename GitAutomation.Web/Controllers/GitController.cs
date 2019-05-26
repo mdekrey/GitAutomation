99e7da7ee29b5d2231c5ede47f4fbeb87933ddcd
Matt DeKrey <mattdekrey@gmail.com>
2019-05-26 12:27:42 -0500

    Add endpoint to get git revisions

---
+++ GitAutomation.Web/Controllers/GitController.cs
@@ -0,0 +1,29 @@
+﻿using GitAutomation.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace GitAutomation.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class GitController : Controller
+    {
+
+        [HttpGet("revision-diffs/{*commitish}")]
+        public async Task<IActionResult> GetTargetLastFetch([FromRoute] string commitish, [FromServices] IOptions<TargetRepositoryOptions> options)
+        {
+            var streams = await PowerShell.Create()
+                .AddUnrestrictedCommand("./Scripts/Repository/gitRevisionComparison.ps1")
+                .BindParametersToPowerShell(new { options.Value.CheckoutPath, revision = commitish })
+                .InvokeAllStreams<JToken, string>(JToken.Parse);
+            return Ok(streams.Success[0]);
+        }
+
+    }
+}
---
+++ GitAutomation.Web/Scripts/Repository/gitRevisionComparison.ps1
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/env pwsh
+
+param(
+	[string] $checkoutPath,
+	[string] $revision
+)
+
+Push-Location "$checkoutPath"
+$result = @()
+(git for-each-ref --format='%(refname) {%(objectname)}' refs/heads | ForEach-Object -Process {
+	$r = [regex]::match($_, '^refs/heads/(?<name>.+) \{(?<commit>[0-9a-f]{40})\}')
+	$commit = $r.Groups['commit'].Value
+	$diff = git rev-list --count --left-right "$($revision)...$($commit)"
+	if ($diff -ne $null)
+	{
+		$r2 = [regex]::match($diff, '^(?<behind>[0-9]+)\s+(?<ahead>[0-9]+)')
+		$result += @(@{ "name" = $r.Groups['name'].Value; "commit" = $commit; "behind" = $r2.Groups['behind'].Value -as [int]; "ahead" = $r2.Groups['ahead'].Value -as [int] })
+	}
+})
+ConvertTo-Json -InputObject $result
+Pop-Location
